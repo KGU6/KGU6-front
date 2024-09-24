6be9bb6cf25a81acdd2a169a2a075dc44f2b5be5
leeleeleeleejun <wnstjr6293@naver.com>
2024-09-24 22:12:02 +0900

    chore: 초기 디렉토리 설정

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import GlobalStyle from "./styles/GlobalStyle.js";
+import Router from "./Router.jsx";
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  )
+}
+
+export default App
---
+++ src/Router.jsx
@@ -0,0 +1,13 @@
+//import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+const Router = () => {
+return <BrowserRouter>
+  <Routes>
+    <Route element={<div/>}/>
+  </Routes>
+</BrowserRouter>
+}
+
+export default Router;
