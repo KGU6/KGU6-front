95cce07d56bae5bde25fea27dbb20d1be8eea676
백지웅 <jw6133@naver.com>
2024-09-25 21:55:39 +0900

    Feat : add GoogleMap API

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,12 @@
 //import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import HomePage from './pages/HomePage/HomePage';
 
 const Router = () => {
 return <BrowserRouter>
   <Routes>
     <Route element={<div/>}/>
+    <Route path='/' element={<HomePage/>}/>
   </Routes>
 </BrowserRouter>
 }
