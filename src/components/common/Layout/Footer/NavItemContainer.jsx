9a0fc3237f4fe96462639bd380286373c28d886e
leeleeleeleejun <wnstjr6293@naver.com>
2024-09-26 23:06:49 +0900

    design: footer svg icon 추가

--- src/components/common/Layout/Footer/NavItemContainer.jsx
+++ src/components/common/Layout/Footer/NavItemContainer.jsx
@@ -1,13 +1,12 @@
 import { NavItem } from './Footer.style.js';
 import { useLocation } from 'react-router-dom';
 
-const NavItemContainer = ({ children, contentName, path }) => {
+const NavItemContainer = ({ children, path }) => {
   const location = useLocation();
 
   return (
     <NavItem to={path} $isActive={path === location.pathname}>
       {children}
-      {contentName && <span>{contentName}</span>}
     </NavItem>
   );
 };
