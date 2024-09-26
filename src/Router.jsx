d523b9aa27f515359729fc7dd32e5c89e7535dae
백지웅 <jw6133@naver.com>
2024-09-27 01:25:40 +0900

    feat:로그인페이지 형태만

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 //import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage';
 
 const Router = () => {
 return <BrowserRouter>
   <Routes>
     <Route element={<div/>}/>
-    <Route path='/' element={<HomePage/>}/>
+    <Route path='/l' element={<HomePage/>}/>
+    <Route path='/' element={<LoginPage/>}/>
   </Routes>
 </BrowserRouter>
 }
