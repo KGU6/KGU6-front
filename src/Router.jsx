f1a9678701ab377a4cacf83df3e79eb6678042fc
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-09-28 06:50:04 +0900

    design : mypage UI

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,21 @@
 //import { lazy, Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchScreen from "./pages/search-screen/SearchScreen";
+import SearchResultScreen from "./pages/search-screen/SearchResultScreen";
+import MyPageScreen from "./pages/MyPageScreen";
 
 const Router = () => {
-return <BrowserRouter>
-  <Routes>
-    <Route element={<div/>}/>
-  </Routes>
-</BrowserRouter>
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<div />} />
+        <Route path="/" element={<SearchScreen />} />
+        <Route path="/result" element={<SearchResultScreen />} />
+        <Route path="/mypage" element={<MyPageScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
