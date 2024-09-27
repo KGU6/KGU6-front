bb53b86c7ca68eab8186ee66337b586cb1a3e13f
leeleeleeleejun <wnstjr6293@naver.com>
2024-09-27 19:38:12 +0900

    feat: 장소 추가하기 기능 - 지도에 마커 생성 - status에 따라 마커 변경 - 마커 생성 시 지도 조정

--- src/components/createPost/Content/index.jsx
+++ src/components/createPost/Content/index.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import 'react-quill/dist/quill.snow.css';
 import {
   IconBox,
@@ -12,36 +11,53 @@ import GoodIcon from '@/assets/icons/good-icon.svg?react';
 import NotBadIcon from '@/assets/icons/not-bad-icon.svg?react';
 import BadIcon from '@/assets/icons/bad-icon.svg?react';
 
-function Content({ place }) {
-  const [value, setValue] = useState('');
-  const [status, setStatus] = useState('GOOD');
+function Content({ id, content, updateContent }) {
   return (
     <>
       <PlaceContainer>
         <LocationIcon />
-        {place}
+        {content.place.name}
         <IconBox>
           <IconButton
-            $isActive={status === 'GOOD'}
-            onClick={() => setStatus('GOOD')}
+            $isActive={content.status === 'GOOD'}
+            onClick={() => {
+              if (content.status !== 'GOOD') {
+                updateContent(id, 'status', 'GOOD');
+              }
+            }}
           >
             <GoodIcon />
-          </IconButton>
+          </IconButton>{' '}
           <IconButton
-            $isActive={status === 'NOT_BAD'}
-            onClick={() => setStatus('NOT_BAD')}
+            $isActive={content.status === 'NOT_BAD'}
+            onClick={() => {
+              if (content.status !== 'NOT_BAD') {
+                updateContent(id, 'status', 'NOT_BAD');
+              }
+            }}
           >
             <NotBadIcon />
           </IconButton>
           <IconButton
-            $isActive={status === 'BAD'}
-            onClick={() => setStatus('BAD')}
+            $isActive={content.status === 'BAD'}
+            onClick={() => {
+              if (content.status !== 'BAD') {
+                updateContent(id, 'status', 'BAD');
+              }
+            }}
           >
             <BadIcon />
           </IconButton>
         </IconBox>
       </PlaceContainer>
-      <Quill theme='snow' value={value} onChange={setValue} modules={modules} />
+      <Quill
+        theme='snow'
+        value={content.value}
+        onChange={(content) => {
+          updateContent(id, 'content', content);
+        }}
+        modules={modules}
+      />
     </>
   );
 }
