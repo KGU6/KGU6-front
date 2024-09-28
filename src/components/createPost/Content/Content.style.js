b45e85a358544c14fefb8f5c087d7975ffaf496d
leeleeleeleejun <wnstjr6293@naver.com>
2024-09-28 13:32:29 +0900

    chore: quill image-upload 패키지 설치 - quill 에디터 이미지 파일 처리 로직 추가

--- src/components/createPost/Content/Content.style.js
+++ src/components/createPost/Content/Content.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill';
 
 export const PlaceContainer = styled.div`
   display: flex;
@@ -37,65 +37,10 @@ export const IconButton = styled.button`
   }
 `;
 
-export const Quill = styled(ReactQuill)`
+export const QuillEdi = styled(ReactQuill)`
   min-height: 150px;
   max-height: 200px;
   width: 100%;
   margin-top: 10px;
   margin-bottom: 80px;
 `;
-
-export const modules = {
-  toolbar: {
-    container: [
-      [{ header: [2, 3, 4, 5, 6, false] }],
-      [{ align: [] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
-      [
-        {
-          color: [
-            '#000000',
-            '#e60000',
-            '#ff9900',
-            '#ffff00',
-            '#008a00',
-            '#0066cc',
-            '#9933ff',
-            '#ffffff',
-            '#facccc',
-            '#ffebcc',
-            '#ffffcc',
-            '#cce8cc',
-            '#cce0f5',
-            '#ebd6ff',
-            '#bbbbbb',
-            '#f06666',
-            '#ffc266',
-            '#ffff66',
-            '#66b966',
-            '#66a3e0',
-            '#c285ff',
-            '#888888',
-            '#a10000',
-            '#b26b00',
-            '#b2b200',
-            '#006100',
-            '#0047b2',
-            '#6b24b2',
-            '#444444',
-            '#5c0000',
-            '#663d00',
-            '#666600',
-            '#003700',
-            '#002966',
-            '#3d1466',
-            'custom-color',
-          ],
-        },
-        { background: [] },
-      ],
-      ['image'],
-    ],
-  },
-};
--- src/components/createPost/Content/index.jsx
+++ src/components/createPost/Content/index.jsx
@@ -2,16 +2,21 @@ import 'react-quill/dist/quill.snow.css';
 import {
   IconBox,
   IconButton,
-  modules,
   PlaceContainer,
-  Quill,
+  QuillEdi,
 } from './Content.style.js';
 import LocationIcon from '@/assets/icons/location-dot-icon.svg?react';
 import GoodIcon from '@/assets/icons/good-icon.svg?react';
 import NotBadIcon from '@/assets/icons/not-bad-icon.svg?react';
 import BadIcon from '@/assets/icons/bad-icon.svg?react';
+import { Quill } from 'react-quill';
+import ImageUploader from 'quill-image-uploader';
+
+Quill.register('modules/imageUploader', ImageUploader);
 
 function Content({ id, content, updateContent }) {
+  console.log(content.content);
+
   return (
     <>
       <PlaceContainer>
@@ -50,7 +55,7 @@ function Content({ id, content, updateContent }) {
           </IconButton>
         </IconBox>
       </PlaceContainer>
-      <Quill
+      <QuillEdi
         theme='snow'
         value={content.value}
         onChange={(content) => {
@@ -63,3 +68,83 @@ function Content({ id, content, updateContent }) {
 }
 
 export default Content;
+
+export const modules = {
+  toolbar: {
+    container: [
+      [{ header: [2, 3, 4, 5, 6, false] }],
+      [{ align: [] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
+      [
+        {
+          color: [
+            '#000000',
+            '#e60000',
+            '#ff9900',
+            '#ffff00',
+            '#008a00',
+            '#0066cc',
+            '#9933ff',
+            '#ffffff',
+            '#facccc',
+            '#ffebcc',
+            '#ffffcc',
+            '#cce8cc',
+            '#cce0f5',
+            '#ebd6ff',
+            '#bbbbbb',
+            '#f06666',
+            '#ffc266',
+            '#ffff66',
+            '#66b966',
+            '#66a3e0',
+            '#c285ff',
+            '#888888',
+            '#a10000',
+            '#b26b00',
+            '#b2b200',
+            '#006100',
+            '#0047b2',
+            '#6b24b2',
+            '#444444',
+            '#5c0000',
+            '#663d00',
+            '#666600',
+            '#003700',
+            '#002966',
+            '#3d1466',
+            'custom-color',
+          ],
+        },
+        { background: [] },
+      ],
+      ['image'],
+    ],
+  },
+  imageUploader: {
+    upload: (file) => {
+      return new Promise((resolve, reject) => {
+        const formData = new FormData();
+        formData.append('image', file);
+
+        fetch(
+          'https://api.imgbb.com/1/upload?key=c93b7d1d3f7a145263d4651c46ba55e4',
+          {
+            method: 'POST',
+            body: formData,
+          }
+        )
+          .then((response) => response.json())
+          .then((result) => {
+            console.log(result);
+            resolve(result.data.url);
+          })
+          .catch((error) => {
+            reject('Upload failed');
+            console.error('Error:', error);
+          });
+      });
+    },
+  },
+};
